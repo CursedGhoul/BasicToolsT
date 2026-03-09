1626e2a88f8f1573c9b9bff4dfeaadb866d88cdf
Ghoul <CursedGhoulBlox@gmail.com>
2026-03-09 14:09:59 -0400

    Update basictools.py

--- src/basictools.py
+++ src/basictools.py
@@ -1,3 +1,18 @@
+import sounddevice as sd
+import soundfile as sf
+
 def title2(text):
     """Returns a superior title case from default title()"""
     return ' '.join(word.capitalize() for word in text.split(' '))
+
+def play_sound(path, stop_terminal=False,debug_statements=False):
+    data, fs = sf.read(path, dtype="float32")
+    if debug_statements == True:
+        print(f"playing {path}")
+    sd.play(data, fs)
+    if stop_terminal == True:
+        if input("Press ENTER to stop the sound: \n") == (""):
+            sd.stop()
+    sd.wait()
+    if debug_statements == True:
+        print("playing finished")
